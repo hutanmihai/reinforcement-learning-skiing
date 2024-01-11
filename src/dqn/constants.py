1f315b85b0e1314ad3539b056bcca086ba5bbe6c
mihaihutan <hutanmihai29@gmail.com>
2024-01-12 00:11:43 +0200

    Continue

--- src/dqn/constants.py
+++ src/dqn/constants.py
@@ -10,10 +10,10 @@ NUM_ACTIONS = 3  # Number of actions the agent can take
 INPUT_SHAPE = (CHANNELS, WINDOW_SIZE, WINDOW_SIZE)  # PyTorch uses (channels, height, width) format
 
 # TODO: Fine tuning
-LEARNING_RATE = 0.001
+LEARNING_RATE = 0.0005
 MIN_MEMORY_CAPACITY = 100_000  # This should be at least BATCH_SIZE
 MEMORY_CAPACITY = 500_000
-NUM_EPISODES = 200
+NUM_EPISODES = 4000
 BATCH_SIZE = 32  # How many samples to take from the replay memory when learning
 UPDATE_FREQUENCY = 10  # How often to update the target network, measured in episodes
 
